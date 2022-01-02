bf92e77e5e05b6447cf0a00c679e5ba29252cd00
Scholberg, Kate <schol@phy.duke.edu>
2022-01-01 20:52:49 -0500

    Update Giunti charge radius smearing; add Tomalek smearing

--- xscns.h
+++ xscns.h
@@ -6,6 +6,9 @@ double diffxscnaxial(double, double, double);
 double diffxscninterf(double, double, double);
 double diffxscnmag(double, double);
 
+double diffnuelectronxscn(int,double, double);
+double diffnuelectronxscn2(int,double, double, double, double, int);
+
 double diffangdist(double, double);
 
 void sm_vector_couplings(int, double*);
@@ -20,6 +23,7 @@ double GV_nsi_nonuniv(int, int, int, double, double, double, double, double, dou
 double GV_nsi_fc2(int, int, int, double, double, double, double, double, double, double, double, double, double);
 
 double chgradcorr(int,int);
+double chgradcorr_tomalak(double,int);
 double mufactor(double);
 double taufactor(double);
 
